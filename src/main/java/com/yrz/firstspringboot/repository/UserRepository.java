2e8915cac81a62632988177b93c1bf13255d21ac
zhailulu <zhailulu123@126.com>
2018-03-22 19:43:36 +0800

    Initial commit

---
+++ src/main/java/com/yrz/firstspringboot/repository/UserRepository.java
@@ -0,0 +1,48 @@
+package com.yrz.firstspringboot.repository;
+
+
+import com.yrz.firstspringboot.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+/***
+ * {@link User}
+ */
+@Repository
+public class UserRepository {
+
+
+    private final ConcurrentHashMap<Integer, User> repository = new ConcurrentHashMap<>();
+
+    private final static AtomicInteger idGenerator = new AtomicInteger();
+
+
+    /**
+     * 保存
+     * @param user
+     * @return
+     */
+    public boolean save(User user) {
+
+        Integer id = idGenerator.incrementAndGet();
+        user.setId(id);
+
+        return repository.put(id, user) == null;
+    }
+
+
+
+    public Collection<User> findAll(){
+
+        return repository.values();
+
+    }
+
+
+
+
+}
